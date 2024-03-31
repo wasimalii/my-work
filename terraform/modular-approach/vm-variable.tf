6b952354c4ba3fa8ad5f799a64b411f7fc09b455
Md wasim <42905470+wasimalii@users.noreply.github.com>
2024-03-31 23:51:24 +0530

    Create vm-variable.tf

---
+++ terraform/modular-approach/vm-variable.tf
@@ -0,0 +1,32 @@
+variable "name" {
+    description = "This is vm name"
+}
+
+variable "machine_type" {
+    description = "This is for machine type"
+}
+
+variable "image" {
+    description = "This is for vm image"
+
+}
+
+variable "size" {
+    description = "This is for boot disk size"
+}
+
+variable "boot_disk_type" {
+    description = "This is for boot disk type"
+}
+
+variable "additional_disk_size" {
+    description = "This is for additional disk size"
+}
+
+variable "disk_name" {
+    description = "This is for additional disk name"
+}
+
+variable "disk_type" {
+    description = "This is for additional disk type"
+}
